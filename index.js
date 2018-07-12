c11d3b2ec8b58040b33b8b4b9bcdc4981e36d49e
kjoewill@gmail.com <kjoewill@gmail.com>
2018-07-12 22:45:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function increaseRankBy(n) {
   }
   
   function deepestChild() {
-    return document.getElementById('grad-node')
+    var grandNode = document.getElementById('grand-node')
+    while 
   }
 }
