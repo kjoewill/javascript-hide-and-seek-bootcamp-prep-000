a771cc050db860a5db88c0a28f89a920fc88f50d
kjoewill@gmail.com <kjoewill@gmail.com>
2018-07-12 22:51:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-function getFirstSelector(selector) {
-  return document.querySelector(selector)
+function getFirstSelector(s) {
+  return document.querySelector(s)
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target')
+}
+
+function increaseRankBy(n) {
+  var list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < list.length; i++) {
+    var num = parseInt(list[i].innerHTML)
+    list[i].innerHTML = num + n 
+  }
+}
+  
+function deepestChild() {
+  var child = document.getElementById('grand-node')
+  while (child.querySelector('div')) {
+    child = child.querySelector('div')
+  }
+  return child
 }
